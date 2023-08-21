81c6bc264a8b027ffe606c16084f56a4ac75dbdb
Félin Aldric <fel1aldric@gmail.com>
2023-08-21 14:33:38 +0300

    [feat] bakend vote et metier

--- src/Controller/PropositionPosteController.php
+++ src/Controller/PropositionPosteController.php
@@ -3,12 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\PropositionPoste;
-use App\Form\PropositionPosteType;
 use App\Repository\PropositionPosteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @Route("/proposition/poste")
@@ -18,71 +17,36 @@ class PropositionPosteController extends AbstractController
     /**
      * @Route("/", name="app_proposition_poste_index", methods={"GET"})
      */
-    public function index(PropositionPosteRepository $propositionPosteRepository): Response
+    public function index(PropositionPosteRepository $propositionPosteRepository): JsonResponse
     {
-        return $this->render('proposition_poste/index.html.twig', [
-            'proposition_postes' => $propositionPosteRepository->findAll(),
-        ]);
+        return $this->json($propositionPosteRepository->findAll());
     }
 
     /**
      * @Route("/new", name="app_proposition_poste_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, PropositionPosteRepository $propositionPosteRepository): Response
+    public function new(Request $request, PropositionPosteRepository $propositionPosteRepository): JsonResponse
     {
         $propositionPoste = new PropositionPoste();
-        $form = $this->createForm(PropositionPosteType::class, $propositionPoste);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $propositionPosteRepository->add($propositionPoste);
-            return $this->redirectToRoute('app_proposition_poste_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('proposition_poste/new.html.twig', [
-            'proposition_poste' => $propositionPoste,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="app_proposition_poste_show", methods={"GET"})
-     */
-    public function show(PropositionPoste $propositionPoste): Response
-    {
-        return $this->render('proposition_poste/show.html.twig', [
-            'proposition_poste' => $propositionPoste,
-        ]);
+        $propositionPosteRepository->add($propositionPoste);
+        return $this->json($propositionPosteRepository->findAll());
     }
 
     /**
      * @Route("/{id}/edit", name="app_proposition_poste_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, PropositionPoste $propositionPoste, PropositionPosteRepository $propositionPosteRepository): Response
+    public function edit(Request $request, PropositionPoste $propositionPoste, PropositionPosteRepository $propositionPosteRepository): JsonResponse
     {
-        $form = $this->createForm(PropositionPosteType::class, $propositionPoste);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $propositionPosteRepository->add($propositionPoste);
-            return $this->redirectToRoute('app_proposition_poste_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('proposition_poste/edit.html.twig', [
-            'proposition_poste' => $propositionPoste,
-            'form' => $form,
-        ]);
+        $propositionPosteRepository->add($propositionPoste);
+        return $this->json($propositionPosteRepository->findAll());
     }
 
     /**
      * @Route("/{id}", name="app_proposition_poste_delete", methods={"POST"})
      */
-    public function delete(Request $request, PropositionPoste $propositionPoste, PropositionPosteRepository $propositionPosteRepository): Response
+    public function delete(Request $request, PropositionPoste $propositionPoste, PropositionPosteRepository $propositionPosteRepository): JsonResponse
     {
-        if ($this->isCsrfTokenValid('delete'.$propositionPoste->getId(), $request->request->get('_token'))) {
-            $propositionPosteRepository->remove($propositionPoste);
-        }
-
-        return $this->redirectToRoute('app_proposition_poste_index', [], Response::HTTP_SEE_OTHER);
+        $propositionPosteRepository->remove($propositionPoste);
+        return $this->json($propositionPosteRepository->findAll());
     }
 }
--- src/Controller/VotePropositionController.php
+++ src/Controller/VotePropositionController.php
@@ -7,7 +7,7 @@ use App\Form\VotePropositionType;
 use App\Repository\VotePropositionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -18,71 +18,36 @@ class VotePropositionController extends AbstractController
     /**
      * @Route("/", name="app_vote_proposition_index", methods={"GET"})
      */
-    public function index(VotePropositionRepository $votePropositionRepository): Response
+    public function index(VotePropositionRepository $votePropositionRepository): JsonResponse
     {
-        return $this->render('vote_proposition/index.html.twig', [
-            'vote_propositions' => $votePropositionRepository->findAll(),
-        ]);
+        return $this->json($votePropositionRepository->findAll());
     }
 
     /**
      * @Route("/new", name="app_vote_proposition_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, VotePropositionRepository $votePropositionRepository): Response
+    public function new(Request $request, VotePropositionRepository $votePropositionRepository): JsonResponse
     {
         $voteProposition = new VoteProposition();
-        $form = $this->createForm(VotePropositionType::class, $voteProposition);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $votePropositionRepository->add($voteProposition);
-            return $this->redirectToRoute('app_vote_proposition_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('vote_proposition/new.html.twig', [
-            'vote_proposition' => $voteProposition,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="app_vote_proposition_show", methods={"GET"})
-     */
-    public function show(VoteProposition $voteProposition): Response
-    {
-        return $this->render('vote_proposition/show.html.twig', [
-            'vote_proposition' => $voteProposition,
-        ]);
+        $votePropositionRepository->add($voteProposition);
+        return $this->json($votePropositionRepository->findAll());
     }
 
     /**
      * @Route("/{id}/edit", name="app_vote_proposition_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, VoteProposition $voteProposition, VotePropositionRepository $votePropositionRepository): Response
+    public function edit(Request $request, VoteProposition $voteProposition, VotePropositionRepository $votePropositionRepository): JsonResponse
     {
-        $form = $this->createForm(VotePropositionType::class, $voteProposition);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $votePropositionRepository->add($voteProposition);
-            return $this->redirectToRoute('app_vote_proposition_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('vote_proposition/edit.html.twig', [
-            'vote_proposition' => $voteProposition,
-            'form' => $form,
-        ]);
+        $votePropositionRepository->add($voteProposition);
+        return $this->json($votePropositionRepository->findAll());
     }
 
     /**
      * @Route("/{id}", name="app_vote_proposition_delete", methods={"POST"})
      */
-    public function delete(Request $request, VoteProposition $voteProposition, VotePropositionRepository $votePropositionRepository): Response
+    public function delete(Request $request, VoteProposition $voteProposition, VotePropositionRepository $votePropositionRepository): JsonResponse
     {
-        if ($this->isCsrfTokenValid('delete'.$voteProposition->getId(), $request->request->get('_token'))) {
-            $votePropositionRepository->remove($voteProposition);
-        }
-
-        return $this->redirectToRoute('app_vote_proposition_index', [], Response::HTTP_SEE_OTHER);
+        $votePropositionRepository->remove($voteProposition);
+        return $this->json($votePropositionRepository->findAll());
     }
 }
